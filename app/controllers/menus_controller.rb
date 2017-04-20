2b25a1aa8314640c5fc1327c472e8c5eb831d800
Aaron Calander <aaroncalander@yahoo.com>
2017-04-19 21:44:52 -0700

    Update all files to be current with original repo

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -7,24 +7,3 @@ class MenusController < ApplicationController
     @desserts = Recipe.where(category: "desserts")
   end
 end
-
-
-# private
-
-
-# def app_rating_total
-#   @totals = []
-#   @appetizers.each do |appetizer|
-#     app_ratings = appetizer.ratings
-#     total = app_ratings.reduce(0) { |sum, rating| sum += rating.value }
-#     @totals << total
-#   end
-#   @top_5 = @totals.sort.last(5)
-#   @top_5_apps = @appetizers.where(@totals.include? @top_5)
-  
-#   # @top_5_apps.each do |appetizer|
-#   #   return appetizer.name
-#   # end
-# end
-
-
